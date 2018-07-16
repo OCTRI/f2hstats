ace2ecbf682c9f4609f235db7583ec4e2d7b2e2a
Amy Yates <yateam@ohsu.edu>
2018-07-16 10:06:08 -0700

    Add test that can interrogate stats db. Summarize valueStrings.

--- src/main/java/org/monarchinitiative/f2hstats/F2hStatsApplication.java
+++ src/main/java/org/monarchinitiative/f2hstats/F2hStatsApplication.java
@@ -16,11 +16,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootApplication
-@ComponentScan({ "org.monarchinitiative.f2hstats", "org.monarchinitiative.fhir2hpo" })
+@ContextConfiguration(classes = ApplicationConfig.class)
 @EntityScan(basePackages = { "org.monarchinitiative.f2hstats", "org.monarchinitiative.fhir2hpo" })
 @EnableJpaRepositories(basePackages = { "org.monarchinitiative.f2hstats" })
 public class F2hStatsApplication implements CommandLineRunner {
