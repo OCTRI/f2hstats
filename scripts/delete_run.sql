98529627823817ca66d65ab44d034d09c08b086a
Amy Yates <yateam@ohsu.edu>
2018-08-28 13:29:54 -0700

    Remove script reference to deleted table.

--- scripts/delete_run.sql
+++ scripts/delete_run.sql
@@ -8,7 +8,6 @@ set @max_pat_id = (select max(id) from stats_run_patient where `stats_run` = @ru
 set @min_ob_id = (select min(id) from stats_run_observation where `patient` >= @min_pat_id and patient <= @max_pat_id);
 set @max_ob_id = (select max(id) from stats_run_observation where `patient` >= @min_pat_id and patient <= @max_pat_id);
 
-delete from observation_loinc where observation >= @min_ob_id && observation <= @max_ob_id;
 delete from method_result where observation >= @min_ob_id && observation <= @max_ob_id;
 delete from stats_run_observation where id >= @min_ob_id && id <= @max_ob_id;
 delete from stats_run_patient where id >= @min_pat_id && id <= @max_pat_id;
